d39eb7c23d47a272f8918e143af801e4fa0feeba
JeroenSverige <77346416+JeroenSverige@users.noreply.github.com>
2025-06-29 22:14:48 +0100

    whatever

--- src/components/PlaceConfigurator.js
+++ src/components/PlaceConfigurator.js
@@ -211,7 +211,7 @@ export default function PlaceConfigurator({
           left: { xs: "20px", sm: "auto" },
           right: { xs: "20px", sm: "0px" },
           width: { xs: "92vw", sm: 380 },
-          height: { xs: "calc(100vh - 20vh)", sm: "100vh" },
+          height: { xs: "calc(100vh - 25vh)", sm: "100vh" },
           pointerEvents: "auto",
           borderRadius: { xs: "25px", sm: "0px"}
         },
